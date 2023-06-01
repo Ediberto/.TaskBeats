ee133a6f71398d27f5f3700b016c15f476aa131c
Ediberto <edibertosilva33@gmail.com>
2023-06-01 01:07:42 -0300

    create ViewMOdel1

---
+++ app/src/main/java/com/example/taskbeats_ediberto/TaskBeatsApplication.kt
@@ -0,0 +1,22 @@
+package com.example.taskbeats_ediberto
+
+import android.app.Application
+import androidx.room.Room
+import com.example.taskbeats_ediberto.data.AppDatabase
+
+class TaskBeatsApplication: Application() {
+
+    //CRIAR A variavel dataBase DE DADOS
+    private lateinit var dataBase: AppDatabase
+    override fun onCreate() {
+        super.onCreate()
+        //CRIAR A BASE DE DADOS
+        dataBase = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "taskbeats-database"
+        ).build()
+    }
+    fun getAppDataBase(): AppDatabase {
+        return dataBase
+    }
+}
---
+++ app/src/main/java/com/example/taskbeats_ediberto/presentation/TaskListViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.taskbeats_ediberto.presentation
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskbeats_ediberto.TaskBeatsApplication
+import com.example.taskbeats_ediberto.data.Task
+import com.example.taskbeats_ediberto.data.TaskDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskListViewModel(private val taskDao: TaskDao) : ViewModel() {
+    //AQUI TEREMOS O LIVEDATE NO VIEWMODEL
+    val taskListLiveData: LiveData<List<Task>> = taskDao.getAll()
+    fun execute(taskAction: TaskAction) {
+        when (taskAction.actionType) {
+            ActionType.DELETE.name -> deleteById(taskAction.task!!.id)
+            ActionType.CREATE.name -> insertIntoDataBase(taskAction.task!!)
+            ActionType.UPDATE.name -> updateIntoDataBase(taskAction.task!!)
+            ActionType.DELETE_ALL.name -> deleteAll()
+        }
+    }
+    //DELETA A TAREFA POR ID
+    private fun deleteById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.deleteById(id)
+        }
+    }
+    private fun insertIntoDataBase(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.insert(task)
+        }
+    }
+    private fun updateIntoDataBase(task: Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.update(task)
+        }
+    }
+    //DELETA TODAS TAREFAS
+    private fun deleteAll() {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskDao.deleteAll()
+        }
+    }
+    //PARA CRIARMOS UM DAO PRECISAMOS CRIAR UM PARAMETRO DE CONSTRUÇÃO
+    companion object {
+        fun create(application: Application): TaskListViewModel {
+            val dataBaseInstance = (application as TaskBeatsApplication).getAppDataBase()
+            val dao = dataBaseInstance.taskDao()
+            return TaskListViewModel(dao)
+        }
+    }
+}
