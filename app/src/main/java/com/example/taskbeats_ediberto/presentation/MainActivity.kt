33f54e01a383d039a1478d30587afbe9bbc3e739
Ediberto <edibertosilva33@gmail.com>
2023-06-18 00:32:33 -0300

    App Buttom Bar

---
+++ app/src/main/java/com/example/taskbeats_ediberto/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taskbeats_ediberto.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taskbeats_ediberto.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
